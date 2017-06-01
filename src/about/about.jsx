8039ac4c803a9a6421ae9cc634900d9617483792
Jonathan Lameira <jlameira@gmail.com>
2017-06-01 16:37:57 -0300

    componentes

---
+++ src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nosa história</h2>
+        <p>lorem ipsum dolor sit amet...</p>
+        <h2>Missão e Valor</h2>
+        <p>lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>lorem ipsum dolor sit amet...</p>
+    </div >
+)
