1d54ac42241670f59a628739aa306e2792bff559
jlameira <jlameira@gmail.com>
2017-06-02 01:03:12 -0300

    Grid if iconButton

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
 export default props => (
-    <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
-            <input id='description' className='form-control' placeholder='Adicione um Tarefa'>
-            </input>
-        </div>
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
-            </button>
-        </div>
-    </div>
+  <div role='form' className='todoForm'>
+    <Grid cols='12 9 10'>
+      <input
+        id='description'
+        className='form-control'
+        placeholder='Adicione um Tarefa'></input>
+
+    </Grid>
+    <Grid cols='12 3 2'>
+      <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+
+    </Grid>
+  </div>
 )
